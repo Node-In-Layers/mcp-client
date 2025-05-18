0426dc228a6077fa4d79af518d6f03f95d4166d6
Mike Cornwell <mike@mikecornwell.com>
2025-05-17 20:21:12 -0400

    feat(init): initial upload

---
+++ src/services.ts
@@ -0,0 +1,42 @@
+import { ServicesContext } from '@node-in-layers/core/index.js'
+import { createOrm, ModelInstanceFetcher } from 'functional-models'
+import { datastoreProvider as restDatastoreProvider } from 'functional-models-orm-mcp'
+import { memoizeValueSync } from '@node-in-layers/core/utils.js'
+import { McpClientServices, McpClientConfig } from './types.js'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const create = (): McpClientServices => {
+  const getDatastoreProvider = memoizeValueSync(
+    (context: ServicesContext<McpClientConfig>) => {
+      return restDatastoreProvider(context.config['@node-in-layers/mcp-client'])
+    }
+  )
+
+  const getModelProps = <
+    TModelOverrides extends object = object,
+    TModelInstanceOverrides extends object = object,
+  >(
+    context: ServicesContext
+  ) => {
+    const myContext = context as ServicesContext<McpClientConfig>
+
+    const datastoreProvider = getDatastoreProvider(myContext)
+
+    const orm = createOrm({
+      datastoreAdapter: datastoreProvider,
+    })
+
+    return {
+      Model: orm.Model,
+      fetcher: orm.fetcher as ModelInstanceFetcher<
+        TModelOverrides,
+        TModelInstanceOverrides
+      >,
+    }
+  }
+  return {
+    getModelProps,
+  }
+}
+
+export { create }
